80e04fa3fd7a2bddbf888d2c12043e46feb07e6c
Rezza Pontoh <email@anda.com>
2024-11-18 20:38:09 +0800

    Menambahkan komponen

--- New folder/react/final-project/src/komponen/Menu/index.js
+++ New folder/react/final-project/src/komponen/Menu/index.js
@@ -18,6 +18,12 @@ const Menu = () => {
             <p>Deskripsi singkat tentang makanan.</p>
             <p>Rp 65.000</p>
           </div>
+          <div className="menu-item">
+            <img src="/makanan3.png" alt="Makanan 3" />
+            <h3>Makanan Spesial 2</h3>
+            <p>Quick & Easy Spaghetti Bolognese</p>
+            <p>Rp 90.000</p>
+          </div>
         </div>
       </section>
     </div>
